72a24de315bfd5bda0b42883ebe9278d12a6fe42
daishek <daichekkal@gmail.com>
2023-07-20 17:42:53 +0100

    fix 🛠️

--- src/data/presentation.ts
+++ src/data/presentation.ts
@@ -13,7 +13,7 @@ type PresentationType = {
 const presentation: PresentationType = {
   title: "Hi, I'am Dai",
   description:
-    "Enthusiastic software engineering student and frontend developer from Algeria. Currently working with NextJS and Typescript. Fascinated by AI and ML. Let's create amazing web experiences together! 🚀💫",
+    "Passionate software engineering student and frontend developer from Algeria. Currently working with NextJS and Typescript. Curious about AI and ML. Let's create amazing web experiences together! 🚀💫",
   mail: "daichekkal@gmail.com",
   social: [
     { label: "Github", url: "https://github.com/daishek" },
