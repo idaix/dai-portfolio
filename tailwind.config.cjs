ab145eaf9c665728e057f1bebbbe7384f1ac40c1
daishek <daichekkal@gmail.com>
2023-08-04 18:46:09 +0100

    🎉 update background

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -15,6 +15,28 @@ module.exports = {
         comfortaa: ["Comfortaa", "cursive"],
         "syne-mono": ["Syne Mono", "monospace"],
       },
+      borderRadius: {
+        dot: "30% 70% 62% 38% / 30% 30% 70% 70% ",
+      },
+      animation: {
+        rad: "rad 5s ease infinite",
+      },
+      keyframes: {
+        rad: {
+          "0%": {
+            borderRadius: "30% 70% 62% 38% / 30% 30% 70% 70%",
+          },
+          "25%": {
+            borderRadius: "58% 42% 18% 82% / 76% 54% 46% 24% ",
+          },
+          "75%": {
+            borderRadius: "76% 24% 42% 58% / 52% 30% 70% 48% ",
+          },
+          "100%": {
+            borderRadius: "30% 70% 62% 38% / 30% 30% 70% 70%",
+          },
+        },
+      },
     },
   },
   plugins: [],
